9dc5a641d6885c5b2a9a35dec313af78d0fa3e2f
Abby Aldridge <abbyaldridge@Abbys-MacBook-Pro.local>
2023-03-28 23:29:46 +0100

    Adding Create test

--- test-app/__tests__/pages/create.test.tsx
+++ test-app/__tests__/pages/create.test.tsx
@@ -1,27 +1,78 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react'
+import { render, fireEvent, screen, waitFor } from '@testing-library/react'
 import Create from '../../src/pages/create';
+import Router from "next/router";
+import { devicesMock } from '../../__mocks__/devicesMock';
 
-jest.mock('next/router', () => ({
-    useRouter() {
-        return ({
-            route: '/',
-            pathname: '',
-            query: '',
-            asPath: '',
-            push: jest.fn(),
-            events: {
-                on: jest.fn(),
-                off: jest.fn()
-            },
-            beforePopState: jest.fn(() => null),
-            prefetch: jest.fn(() => null)
-        });
-    },
+jest.mock("next/router", () => ({
+	push: jest.fn(),
 }));
 
+describe('Create component', () => {
+    const device = devicesMock[0];
 
-test('renders the Create page', () => {
-    render(<Create />)
-    expect(screen.getByText('Create Device')).toBeInTheDocument()
-})
+    test('renders the Create page', () => {
+        render(<Create />)
+        expect(screen.getByText('Create Device')).toBeInTheDocument()
+    })
+
+    // Creating a device functionality tested in __tests__/components/form.test.tsx
+
+    it('should have empty values so the user can create a new device', async () => {
+        render(<Create />);
+
+        const idInput = screen.getByRole('textbox', { name: "ID" });
+        const makeInput = screen.getByRole('textbox', { name: "Make" });
+        const modelInput = screen.getByRole('textbox', { name: "Model" });
+        const statusInput = screen.getByRole('textbox', { name: "Status" });
+        const chipsetInput = screen.getByRole('textbox', { name: "Chipset" });
+        const locationInput = screen.getByRole('textbox', { name: "Location" });
+
+        expect(idInput).toHaveValue("");
+        expect(makeInput).toHaveValue("");
+        expect(modelInput).toHaveValue("");
+        expect(statusInput).toHaveValue("");
+        expect(chipsetInput).toHaveValue("");
+        expect(locationInput).toHaveValue("");   
+    });
+
+    it ('should refresh the page when successful', async () => {
+        global.fetch = jest.fn().mockImplementationOnce(() => Promise.resolve());
+
+        const { getByLabelText } = render(<Create />);
+
+        fireEvent.change(getByLabelText('ID'), { target: { value: device.id } });
+        fireEvent.change(getByLabelText('Make'), { target: { value: device.make } });
+        fireEvent.change(getByLabelText('Model'), { target: { value: device.model } });
+        fireEvent.change(getByLabelText('Status'), { target: { value: device.status } });
+        fireEvent.change(getByLabelText('Location'), { target: { value: device.location } });
+
+        const submitButton = screen.getByRole('button', { name: "Create" });
+        fireEvent.click(submitButton);
+
+        await waitFor(() => expect(fetch).toBeCalledTimes(1));
+        expect(Router.push).toHaveBeenCalledWith("/");
+    })
+
+    it('should handle errors', async () => {
+        global.fetch = jest.fn().mockImplementationOnce(() => Promise.reject('API Error'));
+
+        const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
+
+        const { getByLabelText } = render(<Create />);
+
+        fireEvent.change(getByLabelText('ID'), { target: { value: device.id } });
+        fireEvent.change(getByLabelText('Make'), { target: { value: device.make } });
+        fireEvent.change(getByLabelText('Model'), { target: { value: device.model } });
+        fireEvent.change(getByLabelText('Status'), { target: { value: device.status } });
+        fireEvent.change(getByLabelText('Location'), { target: { value: device.location } });
+
+        // Submit form
+        const submitButton = screen.getByRole('button', { name: "Create" });
+        fireEvent.click(submitButton);
+
+        // Wait for the submit to finish
+        await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
+        expect(consoleErrorSpy).toHaveBeenCalledWith('API Error');
+    });
+});
