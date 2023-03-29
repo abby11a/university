553c32b9f3e53f862e605e87941fddce39180c27
Abby Aldridge <abiald@amazon.com>
2023-03-29 11:53:09 +0100

    Improving sign up/ in and improving css

--- test-app/src/pages/auth/signin.tsx
+++ test-app/src/pages/auth/signin.tsx
@@ -1,59 +1,65 @@
 import { useRef } from "react";
-import { getProviders, getSession, signIn } from "next-auth/react"
+import { getProviders, getSession, signIn } from "next-auth/react";
+import styles from "../../styles/Auth.module.css";
 
 const Signin = () => {
-    const email = useRef("");
-    const password = useRef("");
-    return (
-        <div>
-            <h1>Inventory Manager</h1>
-            <h3>Account Login</h3>
+	const email = useRef("");
+	const password = useRef("");
 
-            <div>
-                <form action="#" >
-                    <label htmlFor="email">Email address
-                        <input
-                            type="text"
-                            id="email"
-                            autoFocus
-                            onChange={(e) => (email.current = e.target.value)}
-                        />
-                    </label>
-                    <label htmlFor="password">Password
-                        <input
-                            type="text"
-                            id="password"
-                            onChange={(e) => (password.current = e.target.value)}
-                        />
-                    </label>
-                    <button
-                        type="button"
-                        onClick={() => signIn("credentials", {
-                            email: email.current, password: password.current
-                        })}
-                    >
-                        Log in
-                    </button>
-                    <a href="/auth/signup">Sign Up</a>
-                </form>
-            </div>
-        </div>
-    )
-}
-export default Signin
+	const submitData = async (e: React.SyntheticEvent) => {
+		e.preventDefault();
+		signIn("credentials", {
+			email: email.current,
+			password: password.current,
+		});
+	};
 
-export async function getServerSideProps(context: { req: any; }) {
-    const { req } = context;
-    const session = await getSession({ req });
-    const providers = await getProviders()
-    if (session) {
-        return {
-            redirect: { destination: "/" },
-        };
-    }
-    return {
-        props: {
-            providers,
-        },
-    }
+	return (
+		<div>
+			<h1 className={styles.h1}>Inventory Manager</h1>
+			<div className={styles.signin}>
+				<h2>Account Login</h2>
+				<form action="#" onSubmit={submitData}>
+					<label htmlFor="email" className={styles.input}>
+						Email address
+						<input
+							type="text"
+							id="email"
+							autoFocus
+							onChange={(e) => (email.current = e.target.value)}
+						/>
+					</label>
+					<label htmlFor="password" className={styles.input}>
+						Password
+						<input
+							type="password"
+							id="password"
+							onChange={(e) =>(password.current = e.target.value)}
+						/>
+					</label>
+					<button
+						className={styles.button}
+						disabled={!email || !password}
+						type="submit"
+						value="Signin"
+					>
+						Sign in
+					</button>
+					<a className={styles.option} href="/auth/signup">
+						Sign Up
+					</a>
+				</form>
+			</div>
+		</div>
+	);
+};
+export default Signin;
+
+export async function getServerSideProps(context: { req: any }) {
+	const { req } = context;
+	const session = await getSession({ req });
+	const providers = await getProviders();
+	if (session) {
+		return { redirect: { destination: "/" } };
+	} return { props: { providers } };
 }
