d950046f85a8167064491a562c878133b1fea63a
Abby Aldridge <abbyaldridge@Abbys-MacBook-Pro.local>
2023-03-29 23:51:44 +0100

    Adding filtering to table

--- test-app/src/pages/index.tsx
+++ test-app/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GetServerSideProps } from 'next';
 import Layout from '../components/Layout';
 import Device, { DeviceProps } from '../components/Device';
@@ -12,6 +12,18 @@ type Props = {
   devices: DeviceProps[]
 }
 
+const filterOptions = [
+  { value: '', label: 'All' },
+  { value: 'id', label: 'ID' },
+  { value: 'make', label: 'Make' },
+  { value: 'model', label: 'Model' },
+  { value: 'chipset', label: 'Chipset' },
+  { value: 'status', label: 'Status' },
+  { value: 'availability', label: 'Availability' },
+  { value: 'location', label: 'Location' },
+  { value: 'farmId', label: 'Farm ID' }
+];
+
 // Returns the table
 const devicesTable = (props: Props) => {
   if (!props.devices) {
@@ -23,8 +35,8 @@ const devicesTable = (props: Props) => {
     )
   } else {
     return (
-      <table 
-      className={styles.table}>
+      <table
+        className={styles.table}>
         <TableHeader />
         {tableBody(props)}
       </table>
@@ -49,7 +61,19 @@ const tableBody = (props: Props) => {
 }
 
 const Devices: React.FC<Props> = (props) => {
-  const { data: session, status } = useSession()
+  const { data: session, status } = useSession();
+  const [search, setSearch] = useState('');
+  const [filterFeature, setFilterFeature] = useState('');
+
+  const filteredDevices = props.devices.filter((item: DeviceProps) => {
+    if (!filterFeature) {
+      return Object.values(item).some((value) =>
+        value ? value.toString().toLowerCase().includes(search.toLowerCase()) : false
+      );
+    } else {
+      return (item as any)[filterFeature].toString().toLowerCase().includes(search.toLowerCase());
+    }
+  });
 
   // Authenticated
   if (status === "authenticated") {
@@ -57,15 +81,23 @@ const Devices: React.FC<Props> = (props) => {
       <Layout>
         <div>
           <h1 className={styles.heading}>Devices</h1>
-          <main>
-            {devicesTable(props)}
+          <main className={styles.devices}>
+            <div className={styles.filter}>
+              <select id="filter" value={filterFeature} onChange={(e) => setFilterFeature(e.target.value)}>
+                {filterOptions.map((option) => (
+                  <option key={option.value} value={option.value}>{option.label}</option>
+                ))}
+              </select>
+              <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+            </div>
+            {devicesTable({ devices: filteredDevices })}
           </main>
         </div>
       </Layout>
     )
 
-  // Unauthenticated
-  } else { 
+    // Unauthenticated
+  } else {
     return (
       <section>
         <div>
