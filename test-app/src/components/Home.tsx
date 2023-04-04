c62cce1274850402452abb7b06c86d18946ce5c5
Abby Aldridge <abiald@amazon.com>
2023-04-04 15:38:19 +0100

    Adding seperate component for home page to make it more readable

---
+++ test-app/src/components/Home.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import Layout from "./Layout";
+import Device, { DeviceProps } from "./Device";
+import { TableHeader } from "@/components/TableHeader";
+import styles from "../styles/device.module.css";
+
+// localhost:3000 (main page); Returns a table of devices unless unauthenticated
+type Props = {
+	devices: DeviceProps[];
+};
+
+// Options to filter the device table with
+const filterOptions = [
+	{ value: "", label: "All" },
+	{ value: "id", label: "ID" },
+	{ value: "make", label: "Make" },
+	{ value: "model", label: "Model" },
+	{ value: "chipset", label: "Chipset" },
+	{ value: "status", label: "Status" },
+	{ value: "availability", label: "Availability" },
+	{ value: "location", label: "Location" },
+	{ value: "farmId", label: "Farm ID" },
+];
+
+// Returns the table
+const devicesTable = (props: Props) => {
+	if (!props.devices) {
+		console.log(props);
+		return <div>No Devices Listed</div>;
+	} else {
+		return (
+			<table className={styles.table}>
+				<TableHeader />
+				<tbody>
+					{props.devices.map((device) => {
+						return <Device key={device.id} device={device} />;
+					})}
+				</tbody>
+			</table>
+		);
+	}
+};
+
+const Home: React.FC<Props> = (props: Props) => {
+	const [search, setSearch] = useState("");
+	const [filterFeature, setFilterFeature] = useState("");
+
+  // Returns list of filtered devices, regardless of case and type
+	const filteredDevices = props.devices.filter((item: DeviceProps) => {
+		if (!filterFeature) {
+			return Object.values(item).some((value) => value ? value.toString().toLowerCase().includes(search.toLowerCase()): false);
+		} else {
+			return (item as any)[filterFeature].toString().toLowerCase().includes(search.toLowerCase())};
+	});
+
+	return (
+		<Layout>
+			<div>
+				<h1 className={styles.heading}>Devices</h1>
+				<main className={styles.devices}>
+					<div className={styles.filter}>
+						<select
+							id="filter"
+							value={filterFeature}
+							onChange={(e) => setFilterFeature(e.target.value)}
+						>
+							{filterOptions.map((option) => (
+								<option key={option.value} value={option.value}>
+									{option.label}
+								</option>
+							))}
+						</select>
+						<input
+							type="text"
+							placeholder="Search"
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+						/>
+					</div>
+					{devicesTable({ devices: filteredDevices })}
+				</main>
+			</div>
+		</Layout>
+	);
+};
+
+export default Home;
--- test-app/src/pages/index.tsx
+++ test-app/src/pages/index.tsx
@@ -1,117 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { GetServerSideProps } from 'next';
-import Layout from '../components/Layout';
-import Device, { DeviceProps } from '../components/Device';
+import { DeviceProps } from '../components/Device';
 import prisma from '../lib/prisma';
-import { TableHeader } from '@/components/TableHeader';
 import { useSession, signIn } from "next-auth/react";
-import styles from "../styles/device.module.css";
+import HomePage from '../components/Home';
+import Signin from './auth/signin';
 
 // localhost:3000 (main page)
-type Props = {
+export type Props = {
   devices: DeviceProps[]
 }
 
-const filterOptions = [
-  { value: '', label: 'All' },
-  { value: 'id', label: 'ID' },
-  { value: 'make', label: 'Make' },
-  { value: 'model', label: 'Model' },
-  { value: 'chipset', label: 'Chipset' },
-  { value: 'status', label: 'Status' },
-  { value: 'availability', label: 'Availability' },
-  { value: 'location', label: 'Location' },
-  { value: 'farmId', label: 'Farm ID' }
-];
-
-// Returns the table
-const devicesTable = (props: Props) => {
-  if (!props.devices) {
-    console.log(props);
-    return (
-      <div>
-        No Devices Listed
-      </div>
-    )
-  } else {
-    return (
-      <table
-        className={styles.table}>
-        <TableHeader />
-        {tableBody(props)}
-      </table>
-    )
-  }
-}
-
-// Returns list of devices in rows <tr><td>
-const tableBody = (props: Props) => {
-  return (
-    <tbody>{
-      props.devices.map((device) => {
-        return (
-          <Device
-            key={device.id}
-            device={device}
-          />
-        )
-      })
-    }</tbody>
-  )
-}
-
-const Devices: React.FC<Props> = (props) => {
+const Index: React.FC<Props> = (props) => {
   const { data: session, status } = useSession();
-  const [search, setSearch] = useState('');
-  const [filterFeature, setFilterFeature] = useState('');
 
-  const filteredDevices = props.devices.filter((item: DeviceProps) => {
-    if (!filterFeature) {
-      return Object.values(item).some((value) =>
-        value ? value.toString().toLowerCase().includes(search.toLowerCase()) : false
-      );
-    } else {
-      return (item as any)[filterFeature].toString().toLowerCase().includes(search.toLowerCase());
-    }
-  });
-
-  // Authenticated
+  // If authenticated return home page
   if (status === "authenticated") {
-    return (
-      <Layout>
-        <div>
-          <h1 className={styles.heading}>Devices</h1>
-          <main className={styles.devices}>
-            <div className={styles.filter}>
-              <select id="filter" value={filterFeature} onChange={(e) => setFilterFeature(e.target.value)}>
-                {filterOptions.map((option) => (
-                  <option key={option.value} value={option.value}>{option.label}</option>
-                ))}
-              </select>
-              <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
-            </div>
-            {devicesTable({ devices: filteredDevices })}
-          </main>
-        </div>
-      </Layout>
-    )
+    return <HomePage {...props}/>
+  }
 
-    // Unauthenticated
-  } else {
+  // Else if unauthenticated return login page
+  else {
     return (
-      <section>
-        <div>
-          <h2 className={styles.heading}>Welcome To Inventory Manager</h2><br />
-          <p className={styles.text}>You currently not authenticated.</p>
-          <button className={styles.button} type="button" onClick={() => signIn()}>
-            Login
-          </button>
-        </div>
-      </section>
+      <Signin/>
     );
   }
 }
 
+// Get list of devices from prisma
 export const getServerSideProps: GetServerSideProps = async () => {
   const devices = await prisma.device.findMany({})
   return {
@@ -119,4 +35,4 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }
 }
 
-export default Devices;
+export default Index;
