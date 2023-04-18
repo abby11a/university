e50cda0d3915f175cac4fac59cfc2c0aa5948156
Abby Aldridge <abiald@amazon.com>
2023-04-18 09:54:13 +0100

    Adding validation to the form to ensure all required fields are not empty

--- test-app/src/components/Form.tsx
+++ test-app/src/components/Form.tsx
@@ -110,6 +110,7 @@ export const Form: React.FC<Props> = ({ deviceValues, farmValues, onSubmit, idUn
           ))}
         </select>
       </label>
+      <text className={(!device.id || !device.make || !device.model || !device.status) ? styles.visable : styles.hidden}>Please fill out all required fields</text>
       <input
         className={styles.button}
         disabled={!device.id || !device.make || !device.model || !device.status}
