1a0f68de8e04fdb33c8317324e73acb75429d730
Abby Aldridge <babsaldrich@gmail.com>
2024-02-17 19:32:58 +0000

    Adding CDK code and pipeline

--- inventory-manager-cdk/lib/inventory-manager-cdk-stack.ts
+++ inventory-manager-cdk/lib/inventory-manager-cdk-stack.ts
@@ -1,16 +1,36 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Cluster, ContainerImage } from 'aws-cdk-lib/aws-ecs';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
+import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
+
+const VPC_NAME = "InventoryManagerVpc";
+const CLUSTER_NAME = "InventoryManagerCluster";
+const ECR_NAME = "InventoryManagerEcrRepo";
+const FARGATE_NAME = "InventoryManagerFargateService";
 
 export class InventoryManagerCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a VPC
+    const vpc = new Vpc(this, VPC_NAME, { maxAzs: 2, vpcName: VPC_NAME, });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InventoryManagerCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create an ECS cluster
+    const cluster = new Cluster(this, CLUSTER_NAME, { vpc, clusterName: CLUSTER_NAME});
+
+    // Create an ECR repository
+    const ecrRepository = new Repository(this, ECR_NAME);
+
+    // Define a Fargate Task running on the cluster, using the image from ECR
+    const fargateService = new ApplicationLoadBalancedFargateService(this, FARGATE_NAME, {
+      cluster,
+      cpu: 256,
+      desiredCount: 1,
+      taskImageOptions: { image: ContainerImage.fromEcrRepository(ecrRepository) },
+      memoryLimitMiB: 512,
+      publicLoadBalancer: true
+    });
   }
 }
