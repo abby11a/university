c45afd957d17cfaa985007251cecfb76ef63be8d
Abby Aldridge <babsaldrich@gmail.com>
2024-02-17 18:05:41 +0000

    Initial CDK commit

---
+++ inventory-manager-cdk/test/inventory-manager-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InventoryManagerCdk from '../lib/inventory-manager-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/inventory-manager-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InventoryManagerCdk.InventoryManagerCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
