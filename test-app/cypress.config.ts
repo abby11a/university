9ac53d5198f3f1a679da99577ac3b9acb66dd79e
Abby Aldridge <babsaldrich@gmail.com>
2024-03-05 10:58:53 +0000

    Adding retries and increasing timeout for flaky e2e tests

--- test-app/cypress.config.ts
+++ test-app/cypress.config.ts
@@ -5,5 +5,12 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    retries: {
+      // Configure retry attempts for different modes
+      runMode: 2, // Number of test retries to run in runMode (CI environments)
+      openMode: 0, // Number of test retries in openMode (during cypress open)
+    },
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000
   },
 });
